e3f51361cfe515ca7f4d79a885e45dc3696be7c5
shefali <shefali.s@mishainfotech.com>
2021-12-26 20:32:38 +0530

    add function

--- app/src/main/java/com/mipl/appletslearngit/MainActivity.java
+++ app/src/main/java/com/mipl/appletslearngit/MainActivity.java
@@ -3,6 +3,7 @@ package com.mipl.appletslearngit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Sample();
+    }
+
+    private void Sample(){
+        Toast.makeText(getApplicationContext(), "test Git", Toast.LENGTH_SHORT).show();
     }
 }
